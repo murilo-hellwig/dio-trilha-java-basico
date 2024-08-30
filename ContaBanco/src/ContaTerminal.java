c63fbd768e6e94973701347613673db1604ba4e9
murilo-hellwig <murilo.hellwig@gmail.com>
2024-08-30 20:10:00 -0300

    primeira montagem do código, falta aprimorar

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,3 +1,35 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     
+    public static void main (String [] args) throws Exception {
+        // TO_DO: Conhecer e importar a classe Scanner
+
+        // Exibir as mensagens para o nosso usuário
+
+        // Exibir a mensagem "conta criada"
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        // não sei bem onde posicionar o "saldo" dentro do código, ou se era pro usuário inputar essa informação
+
+        //saldo = 301.15;
+
+        System.out.println("Digite o seu nome completo: ");
+        String nomeCliente = scanner.next();
+        System.out.println("Digite o número da sua agência: ");
+        int agencia = scanner.nextInt();
+        System.out.println("Digite o número da sua conta: ");
+        // a conta não pode ser double, tem que ser numero-digito, ainda não sei como fazer isso
+        //double conta = scanner.nextDouble();
+
+
+        // até aqui funcionou o input dos dados
+        // Abaixo vou imprimir tudo junto pra ver se inputou certo
+
+        System.out.println("Olá, " + nomeCliente + ". O saldo da sua " + agencia+conta + " é ");
+
+
+    }
+
 }
